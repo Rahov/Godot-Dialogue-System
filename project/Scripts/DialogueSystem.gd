b34c021bdcf2f93ebb0c4bf524d8144af16fc0e7
Rahov <georgirohov@gmail.com>
2020-02-04 19:59:47 +0000

    ???

--- project/Scripts/DialogueSystem.gd
+++ project/Scripts/DialogueSystem.gd
@@ -9,22 +9,26 @@ var branch = 0
 func _ready():
 	get_parent().connect("choice_selected", self, "set_choice")
 
-func dialogue(script):
+func _on_Button_pressed():
+	dialogue()
+
+func dialogue():
 	dialogue = [{root = -1, messages = [], branches = []}]
 	branch_stack = []
 	branch = 0
 	
-	var _dialogue = Node.new()
-	_dialogue.set_script(load("res://Resources/Dialogues/" + script + ".vs"))
-	add_child(_dialogue)
+	var _dialogue = $"../../DialogueTest"
+	_dialogue.set_script(load("res://Dialogues/Uganda2.vs"))
+	_dialogue._ready()
 	
-	_dialogue.queue_free()
-	get_parent().start_dialogue(dialogue)
+	print(dialogue)
+#	_dialogue.queue_free()
+#	get_parent().start_dialogue(dialogue)
 
-func show_message(_speaker, _message):
-	dialogue[branch].messages.append({speaker = _speaker, message = _message})
+func show_message(_name, _narration):
+	dialogue[branch].messages.append({speaker = name, message = _narration})
 
-func show_choice(_speaker, question, choice1, choice2, choice3):
+func show_choice(name, _narration, choice1, choice2, choice3):
 	var _choices = []
 	if choice1 != "Null": _choices.append(choice1)
 	if choice2 != "Null": _choices.append(choice2)
@@ -32,12 +36,13 @@ func show_choice(_speaker, question, choice1, choice2, choice3):
 #	if choice4 != "Null": _choices.append(choice4)
 	
 	for i in range(_choices.size()):
+		print(dialogue)
 		dialogue[branch].branches.append(dialogue.size())
 		dialogue.append({root = branch, messages = []})
 	
 	branch_stack.append(branch)
 	dialogue[branch].counter = _choices.size()
-	dialogue[branch].messages.append({speaker = _speaker, message = question, choices = _choices})
+	dialogue[branch].messages.append({speaker = name, message = _narration, choices = _choices})
 
 func on_choice(i):
 	branch = dialogue[branch_stack.back()].branches[i]
