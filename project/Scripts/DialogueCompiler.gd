c7266154ef54f6b2f8cc3b23a432386c5bf57fed
Rahov <georgirohov@gmail.com>
2020-02-29 18:15:27 +0000

    yey

--- project/Scripts/DialogueCompiler.gd
+++ project/Scripts/DialogueCompiler.gd
@@ -3,7 +3,6 @@ extends Node
 
 func _on_Button_pressed():
 	$"../../DialogueTest"._ready()
-	print(dialogue)
 	$"../Panel"._get_dialogue(dialogue)
 
 func _on_Button2_pressed():
@@ -22,20 +21,14 @@ var dialogue : Dictionary = {}
 func conversation(_id, _name, _icon, _narration, _dialogue):
 	dialogue[_id] = {NAME = _name, ICON = _icon, NARR = _narration, CONVO = _dialogue}
 
-func show_choice(_id, _name, _icon, _narration, _dialogue, _choice1, _choice2, _choice3):
-	var _choices = []
-	if _choice1 != "Null": _choices.append(tr(_choice1))
-	if _choice2 != "Null": _choices.append(tr(_choice2))
-	if _choice3 != "Null": _choices.append(tr(_choice3))
-	#if _choice4 != "Null": _choices.append(tr(_choice4))
-	
-	dialogue[_id] = {NAME = _name, ICON = _icon, NARR = _narration, CONVO = _dialogue, CHOICE = _choices}
-
-func repeat(_id, _name, _icon, _narration, _dialogue, _choice1, _choice2, _choice3, _exit):
+func show_choice(_id, _name, _icon, _narration, _dialogue, _choice1, _choice2, _choice3, _exit):
 	var _choices = []
 	if _choice1 != "Null": _choices.append(tr(_choice1))
 	if _choice2 != "Null": _choices.append(tr(_choice2))
 	if _choice3 != "Null": _choices.append(tr(_choice3))
 	if _exit != "Null": _choices.append(tr(_exit))
 	
-	dialogue[_id] = {NAME = _name, ICON = _icon, NARR = _narration, CONVO = _dialogue, CHOICE = _choices, EXIT = _exit}
+	dialogue[_id] = {NAME = _name, ICON = _icon, NARR = _narration, CONVO = _dialogue, CHOICE = _choices}
+
+func return_to_conc(_id, _destination):
+	dialogue[_id] = {DEST = _destination}
--- project/Scripts/DialogueInterpreter.gd
+++ project/Scripts/DialogueInterpreter.gd
@@ -7,11 +7,17 @@ onready var C3 : Button = $"C3"
 
 var dialogue : Dictionary
 var step : String
-var step_number : int
 
+func _get_dialogue( _block : Dictionary):
+	dialogue = _block
+	step = _dialogue_system("init")
 
 func _dialogue_system(_node_ID : String):
 	var _buttons = [C1, C2, C3]
+	if ERROR_index(_node_ID):
+		return ERR_DOES_NOT_EXIST
+	elif dialogue[_node_ID].has("DEST"):
+		_node_ID = dialogue[_node_ID].DEST
 	$"Name".text = dialogue[_node_ID].NAME
 	$"Narr".text = dialogue[_node_ID].NARR
 	$"Convo".text = dialogue[_node_ID].CONVO
@@ -25,11 +31,18 @@ func _dialogue_system(_node_ID : String):
 			_buttons[i].hide()
 	return _node_ID
 
-func _get_dialogue( _block : Dictionary):
-	dialogue = _block
-	step = _dialogue_system("init")
-	print(step)
 
+func ERROR_index(_sample):
+	var check_list = []
+	for i in dialogue.keys():
+		check_list.append(i)
+	if !check_list.has(_sample):
+		print("Index value: *", _sample, "* is not resolvable")
+		return true
+	return false
+
+
+var step_number : int
 func _on_Panel_gui_input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		step_number += 1
@@ -44,4 +57,9 @@ func _on_C1_pressed():
 func _on_C2_pressed():
 	_dialogue_system(C2.text)
 	step = C2.text
+	step_number = 0
+
+func _on_C3_pressed():
+	_dialogue_system(C3.text)
+	step = C3.text
 	step_number = 0
