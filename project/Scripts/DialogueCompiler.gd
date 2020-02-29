f976842f612aec0bdc92ef75acd933e8d908d5be
Rahov <georgirohov@gmail.com>
2020-02-29 14:00:36 +0000

    done

--- project/Scripts/DialogueCompiler.gd
+++ project/Scripts/DialogueCompiler.gd
@@ -3,6 +3,7 @@ extends Node
 
 func _on_Button_pressed():
 	$"../../DialogueTest"._ready()
+	print(dialogue)
 	$"../Panel"._get_dialogue(dialogue)
 
 func _on_Button2_pressed():
@@ -29,3 +30,12 @@ func show_choice(_id, _name, _icon, _narration, _dialogue, _choice1, _choice2, _
 	#if _choice4 != "Null": _choices.append(tr(_choice4))
 	
 	dialogue[_id] = {NAME = _name, ICON = _icon, NARR = _narration, CONVO = _dialogue, CHOICE = _choices}
+
+func repeat(_id, _name, _icon, _narration, _dialogue, _choice1, _choice2, _choice3, _exit):
+	var _choices = []
+	if _choice1 != "Null": _choices.append(tr(_choice1))
+	if _choice2 != "Null": _choices.append(tr(_choice2))
+	if _choice3 != "Null": _choices.append(tr(_choice3))
+	if _exit != "Null": _choices.append(tr(_exit))
+	
+	dialogue[_id] = {NAME = _name, ICON = _icon, NARR = _narration, CONVO = _dialogue, CHOICE = _choices, EXIT = _exit}
