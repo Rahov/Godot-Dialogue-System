79a274f7b718da6702cb3eb46d8d5961f322707d
Rahov <georgirohov@gmail.com>
2020-03-01 19:21:03 +0000

    Dialogue system improvements

--- Scripts/DialogueCompiler.gd
+++ Scripts/DialogueCompiler.gd
@@ -1,22 +1,6 @@
 extends Node
 
 
-func _on_Button_pressed():
-	$"../../DialogueTest"._ready()
-	$"../Panel"._get_dialogue(dialogue)
-
-func _on_Button2_pressed():
-	var test_vs = $"../../DialogueTest"
-	test_vs.set_script(load("res://Dialogues/1Node.vs"))
-	test_vs.name_changer = "changed"
-	
-
-func _on_Button3_pressed():
-	$"../../DialogueTest".set_script(load("res://Dialogues/2Node.vs"))
-	
-
-
-
 var dialogue : Dictionary = {}
 func conversation(_id, _name, _icon, _narration, _dialogue):
 	dialogue[_id] = {NAME = _name, ICON = _icon, NARR = _narration, CONVO = _dialogue}
@@ -32,3 +16,17 @@ func show_choice(_id, _name, _icon, _narration, _dialogue, _choice1, _choice2, _
 
 func return_to_conc(_id, _destination):
 	dialogue[_id] = {DEST = _destination}
+
+func status(_script : String, _choice : String, _change : String):
+	if $"../Panel".conversations[_script].has(_choice):
+		return _change
+
+func _on_Button_pressed():
+	$"../../DialogueTest"._ready()
+	$"../Panel"._get_dialogue(dialogue)
+
+func _on_Button2_pressed():
+	$"../../DialogueTest".set_script(load("res://Dialogues/1Node.vs"))
+
+func _on_Button3_pressed():
+	$"../../DialogueTest".set_script(load("res://Dialogues/3Node.vs"))
--- Scripts/DialogueInterpreter.gd
+++ Scripts/DialogueInterpreter.gd
@@ -14,16 +14,16 @@ func _get_dialogue( _block : Dictionary):
 	step = _dialogue_system("init")
 
 func _dialogue_system(_node_ID : String):
-	if ERROR_index(_node_ID):
+	if ERROR_index(dialogue, _node_ID):
 		return ERR_DOES_NOT_EXIST
 	elif dialogue[_node_ID].has("DEST"):
 		_node_ID = dialogue[_node_ID].DEST
 		
-	var _buttons = [C1, C2, C3, C4]
 	$"Name".text = dialogue[_node_ID].NAME
 	$"Narr".text = dialogue[_node_ID].NARR
 	$"Convo".text = dialogue[_node_ID].CONVO
 
+	var _buttons = [C1, C2, C3, C4]
 	if dialogue[_node_ID].has("CHOICE"):
 		for i in dialogue[_node_ID].CHOICE.size():
 			_buttons[i].text = dialogue[_node_ID].CHOICE[i]
@@ -53,15 +53,25 @@ func _on_C4_pressed():
 	button_pressed(C4.text)
 
 func button_pressed(_text : String):
+	dialogue_database($"../../DialogueTest".get_script().get_path().get_file(), _text)
 	_dialogue_system(_text)
 	step = _text
 	step_number = 0
 
-func ERROR_index(_sample):
-	var check_list = []
-	for i in dialogue.keys():
-		check_list.append(i)
-	if !check_list.has(_sample):
+var conversations : Dictionary
+var choices : Array
+func dialogue_database(_script : String, _choice_entry : String):
+	if choices.has(_choice_entry):
+		return 0
+	choices.append(_choice_entry)
+	conversations[_script] = choices
+	print(conversations)
+
+func ERROR_index(_struct : Dictionary, _sample : String):
+	var _check_list = []
+	for i in _struct.keys():
+		_check_list.append(i)
+	if !_check_list.has(_sample):
 		print("Index value: *", _sample, "* is not resolvable")
 		return true
 	return false
